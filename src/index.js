88aa03d2f4b9b8c513be1056d4dab4e9b6d7c9ee
xiaobin.kuang <xiaobin.kuang@hstong.com>
2021-06-15 00:22:54 +0800

    chore: 更新文档

--- src/index.js
+++ src/index.js
@@ -11,8 +11,3 @@ lifecycleMixin(Vue);
 renderMixin(Vue);
 
 export default Vue;
-
-// npm 发包文档
-// https://www.icode9.com/content-4-788551.html
-// https://www.jianshu.com/p/2b857cbd1d4a
-// https://blog.csdn.net/weixin_45047039/article/details/109890779
